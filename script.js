f1e0d00489ba0b41d152d787c4d860f039cb08c7
akin <akin@femka.de>
2025-10-22 14:37:09 +0200

    added new images with different file extension, caused problems; adjusted styles

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-//TODO
-//Erstelle die Struktur für das HTML Dokument
-// Header mit Logo
-// favicon einbinden
-//Footer mit Copyright und kleinem Logo
-//CSS mit Variablen für die globale Anwendung auf meine Elemente
-// alles responsive bis 320px Breite muss ohne Scrollbalken sein
-
-
-//Im Script
-// Funktion für das rendern/anzeigen der Bücher mit Titel, Bild, Likes, Preis und Kommentaren: Achtung: verschachtelte for-Schleife aufgrund der Kommentare (eigenes Array)
-//OPTIONAL: Funktion für das Speichern im LocalStorage
-// Funktion für das Hinzufügen von Kommentaren
-// Funktion für Likes. Hier muss sich die Herzfarbe zu rot färben, wenn man liked (entweder mit Bild oder andere Methode display: none oder so etwas in der Art also
-// zwei Bilder austauschen lassen)
-//
-	
+"use strict";
 
 let books = [
 	{
@@ -211,7 +195,140 @@ let books = [
 	},
 ];
 
-function list() {
-	let contentRef = document.getElementById()
+let savedBooks = localStorage.getItem("books");
+if (savedBooks) {
+	books = JSON.parse(savedBooks);
+}
+
+function renderBooks() {
+	let contentRef = document.getElementById("books-container");
+
+	for (let indexBooks = 0; indexBooks < books.length; indexBooks++) {
+		contentRef.innerHTML += getHTMLForBooks(indexBooks);
+		let ulRef = document.getElementById(`ul${indexBooks}`);
+		for (
+			let indexComments = 0;
+			indexComments < books[indexBooks].comments.length;
+			indexComments++
+		) {
+			ulRef.innerHTML += getHTMLForComments(indexBooks, indexComments);
+		}
+	}
+}
+
+function getHTMLForBooks(indexBooks) {
+	return `<div class="card">
+                    <h2 class="bold">${books[indexBooks].name}</h2>
+                    <div class="card-content">
+                        <p class="author-row">
+  						<span class="bold">Autor:</span> ${books[indexBooks].author}
+  						<div class="like">	
+							<img class="heart-icon" id="heart${indexBooks}" 
+       						src="${
+									books[indexBooks].liked === true
+										? "./assets/imgs/heart-filled.png"
+										: "./assets/imgs/heart-empty.png"
+								}"}
+       						onclick="toggleLike(${indexBooks})"
+       						alt="Like Button">
+  							<span id="likes-count${indexBooks}">${books[indexBooks].likes}</span>
+						</div>
+						</p>
+                        <p><span class="bold">Preis:</span> ${books[
+									indexBooks
+								].price
+									.toFixed(2)
+									.replace(".", ",")} €</p>
+                        <p><span class="bold">Veröffentlichung:</span> ${
+									books[indexBooks].publishedYear
+								}</p>
+                        <p><span class="bold">Genre:</span> ${
+									books[indexBooks].genre
+								}</p>
+                        <button id="btn${indexBooks}" class="dropdown-btn" onclick="toggleComments(${indexBooks})">
+                        	Kommentare anzeigen
+                        </button>
+                        <ul id="ul${indexBooks}" class="comments-list hidden">
+  							<li class="comment-form">
+								<input type="text" id="name${indexBooks}" placeholder="Dein Name">
+								<textarea id="comment${indexBooks}" placeholder="Dein Kommentar"></textarea>
+								<button onclick="addComment(${indexBooks})">Kommentar hinzufügen</button>
+  							</li>
+						</ul>
+                    </div>    
+	`;
+}
+
+function getHTMLForComments(indexBooks, indexComments) {
+	return `
+		<li>${books[indexBooks].comments[indexComments].name}: ${books[indexBooks].comments[indexComments].comment}</li>
+	`;
+}
+
+function toggleComments(indexBooks) {
+	let ulRef = document.getElementById(`ul${indexBooks}`);
+	let btnRef = document.getElementById(`btn${indexBooks}`);
+
+	ulRef.classList.toggle("hidden");
+
+	if (ulRef.classList.contains("hidden")) {
+		btnRef.textContent = "Kommentare anzeigen";
+	} else {
+		btnRef.textContent = "Kommentare verbergen";
+	}
+}
+
+function toggleLike(indexBooks) {
+	if (books[indexBooks].liked === false) {
+		books[indexBooks].liked = true;
+		books[indexBooks].likes += 1;
+	} else {
+		books[indexBooks].liked = false;
+		books[indexBooks].likes -= 1;
+	}
+
+	let heartImg = document.getElementById(`heart${indexBooks}`);
+	heartImg.src =
+		books[indexBooks].liked === true
+			? "./assets/imgs/heart-filled.png"
+			: "./assets/imgs/heart-empty.png";
+
+	// Likes-Zahl aktualisieren
+	document.getElementById(`likes-count${indexBooks}`).textContent =
+		books[indexBooks].likes;
 	
+	localStorage.setItem("books", JSON.stringify(books));
+
+}
+
+function addComment(indexBooks) {
+	let newCommentName = document.getElementById(`name${indexBooks}`).value;
+	let newCommentText = document.getElementById(`comment${indexBooks}`).value;
+
+	if (newCommentName.trim() !== "" && newCommentText.trim() !== "") {
+
+		let newCommentObject = {
+			"name": newCommentName,
+			"comment": newCommentText
+		};
+		books[indexBooks].comments.unshift(newCommentObject);
+
+		document.getElementById(`name${indexBooks}`).value = "";
+		document.getElementById(`comment${indexBooks}`).value = "";
+
+		localStorage.setItem("books", JSON.stringify(books));
+
+		let ulRef = document.getElementById(`ul${indexBooks}`);
+		ulRef.innerHTML = `<li class="comment-form">
+  		<input type="text" id="name${indexBooks}" placeholder="Dein Name">
+  		<textarea id="comment${indexBooks}" placeholder="Dein Kommentar"></textarea>
+  		<button onclick="addComment(${indexBooks})">Kommentar hinzufügen</button>
+		</li>`;
+		for (let i = 0; i < books[indexBooks].comments.length; i++) {
+			ulRef.innerHTML += getHTMLForComments(indexBooks, i);
+		}
+	} else {
+		console.log("Bitte füllen Sie beide Felder aus!");
+	}
+
 }
